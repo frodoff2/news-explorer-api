c41e0efb47df4898bc379fcfea38d69ffa9696e7
Asset Murzabayev <assetmurzabayev@MacBook-Pro-Asset-2.local>
2020-12-13 22:28:22 +0300

    s

--- app.js
+++ app.js
@@ -20,12 +20,7 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger); // подключаем логгер запросов
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 // роуты, не требующие авторизации
 app.post('/signup', userValidation, postUser);
 app.post('/signin', loginValidation, login);
