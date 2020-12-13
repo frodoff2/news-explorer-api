4d49c7e572fee6df628334ab40f8f425e212d68a
Asset Murzabayev <assetmurzabayev@MacBook-Pro-Asset-2.local>
2020-12-13 17:37:59 +0300

    sdf

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
+app.use((req, res, next) => { res.append('Access-Control-Allow-Origin', ['*']); res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); res.append('Access-Control-Allow-Headers', 'Content-Type'); next(); });
+
 app.use(errors()); // обрабатывает celebreate
 
 app.use(serverError); // централизованная ошибка
